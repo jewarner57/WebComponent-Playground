d9e0091a460b975f2249ada4c0b2803d6ed4a789
Jonathan Warner <jewarner57@gmail.com>
2021-09-30 20:23:24 -0400

    feat: complete challenge 2 - rainbow

--- 01-simple-component/02-rainbow-text/rainbow-text.js
+++ 01-simple-component/02-rainbow-text/rainbow-text.js
@@ -4,22 +4,42 @@ class rainbowText extends HTMLElement {
     super();
 
     this._shadowRoot = this.attachShadow({ mode: 'open' });
-    
+
     // Get the text of the host element this.innerHTML
+    this._text = this.innerHTML
 
     // Split the string into an array of words text.split(' ')
+    this._textArray = this._text.split(' ')
+
+    this._phrase = document.createElement('p')
 
     // Loop over each word in the array
+    this._textArray.forEach((elem, index) => {
+      const letters = elem.split('')
+      const word = document.createElement('span')
+      word.style.margin = '5px'
 
-      // Make a span 
+      letters.forEach((elem, index) => {
 
-      // Set the innerHTML of the span to the current word 
+        // Make a span 
+        let letter = document.createElement('span')
+        letter.style.fontSize = '2em'
+        letter.style.margin = '-2px'
+        // Set the innerHTML of the span to the current word 
+        letter.innerHTML = elem + ' '
+        // Set the color of the span: span.style.color
+        const hue = 360 % this._text.length * index
+        letter.style.color = `hsl(${hue}, 70%, 50%)`
 
-      // Set the color of the span: span.style.color
+        // Append the word to the shadowroot
+        word.appendChild(letter)
+      });
+      this._phrase.appendChild(word)
 
-      // Append the word to the shadowroot
+    })
 
-    
+    this._shadowRoot.appendChild(this._phrase)
+
   }
 }
 
@@ -27,14 +47,14 @@ customElements.define('rainbow-text', rainbowText);
 
 /*
 
-- Challenge - 1 - 
+- Challenge - 1 -
 
 All of the words in the 'rainbow-text' tag should each be a different
 color. To make this happen you'll need to get the text from the source
-tag, split into words, make a span for each word and style each of 
-these spans with a color. 
+tag, split into words, make a span for each word and style each of
+these spans with a color.
 
-Follow these steps: 
+Follow these steps:
 
 - Get the text from the source tag with this.innerHTML
 - Split this into an array of words with split(' ')
@@ -44,14 +64,14 @@ Follow these steps:
   - Set the style to a color: el.style.color = someColor
   - Append the span to the shadowRoot: this._shadowRoot.appendChild(el)
 
-To set the color you can use one of these methods. 
+To set the color you can use one of these methods.
 
-Set the color with something like this: 
+Set the color with something like this:
 
 const hue = 360 / words.length * i
 const color = `hsl(${hue}, 100%, 50%)`
 
-- Challenge - 2 - 
+- Challenge - 2 -
 
 Modify the code to set the color of each letter.
 
