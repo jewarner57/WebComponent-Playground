00f85a3a016ec97cb1120ca16dfc4b5418784c5a
Jonathan Warner <jewarner57@gmail.com>
2021-10-04 16:03:18 -0400

    feat: finish slideshow challenge

--- 01-simple-component/07-simple-slides/js/simple-slides.js
+++ 01-simple-component/07-simple-slides/js/simple-slides.js
@@ -23,10 +23,12 @@ The goal is to be able to stop and start the slideshow.
 
 class SimpleSlides extends HTMLElement {
   constructor() {
-    super() 
-    
+    super()
+
     // Use this.getAttribute to get the width, height, time, and transition
     this._width = this.getAttribute('width')
+    this._height = this.getAttribute('height')
+    this._time = this.getAttribute('time')
 
 
     // Create a shadow root node
@@ -37,22 +39,31 @@ class SimpleSlides extends HTMLElement {
     // Set the width, height, border, and overflow
     // Append container to shadowroot
     this._container = document.createElement('div')
+    this._container.style.width = this._width
+    this._container.style.height = this._height
+    this._container.style.overflow = 'hidden'
 
-    
+    this._shadowRoot.appendChild(this._container)
+
     // Create an inner div
-    // Add some styles display: flex, 
-    // set transition to the value you got from the attribute add 'ms'
-    // Append this to container
+    this._inner = document.createElement('div')
+    this._inner.style.display = 'flex'
+    this._inner.style.transition = `${this._time}ms`
 
+    // Append this to container
+    this._container.appendChild(this._inner)
 
     // Get array of images
     this._imgs = Array.from(this.querySelectorAll('img'))
     // loop over all of the the images in the array and append then to the inner div
+    this._imgs.forEach((img) => {
+      this._inner.appendChild(img)
+    })
 
-    
     // Keep track of the index of the current image displayed
     this._index = 0
-    
+
+    this._paused = false
   }
 
   // Use this add the interval/timer
@@ -60,53 +71,64 @@ class SimpleSlides extends HTMLElement {
     // use setInterval with the interval set to the time you got from the attribute
     // The callback should call this._nextImg() 
     // Save a reference to your timer so you can remove it
-    
+    this._timer = setInterval(() => {
+      if (!this._paused) {
+        this._nextImg()
+      }
+    }, this._time);
+
   }
 
   // Use this method to remove the interval/timer
   _removeTimer() {
     // use clearInterval() to remove your interval. You need to include the timer reference
-
+    clearInterval(this._timer)
   }
 
   // This method is called when this component is added to the DOM
   connectedCallback() {
     // Call this._addTimer() here 
-
+    this._addTimer()
   }
 
   // This method is called when this component is removed from the DOM
   disconnectedCallback() {
     // Call this._removeTimer() here. 
+    this._removeTimer()
   }
-  
+
   // Use this list all properties that your element observes 
   static get observedAttributes() {
     // List the properties you component is observing here: 
     // time, transition...
-    return []
+    return ['width', 'height', 'time', 'transition', 'paused']
   }
 
   // Use this function handle changes to attributes on the custom element
   attributeChangedCallback(name, oldValue, newValue) {
     // Set properties when attributes change
-    switch(name) {
-      case 'time': 
+    switch (name) {
+      case 'time':
         // set your time property
         // remove your timer 
         // add a new timer
-        
+        this._time = newValue
+        this._removeTimer()
         break
-    
-      case 'transition':
+
+      case 'time':
         // Set your transition property
         // Set the transition style on the inner div be sure to 'ms'
-        
-        break 
+        this._time = newValue
+        break
 
-      case 'paused': 
+      case 'paused':
         // Stretch challenge Set a pause attribute 
+        this._paused = JSON.parse(newValue)
+        break
 
+      case 'transition':
+        this._transition = newValue
         break
 
     }
@@ -115,9 +137,16 @@ class SimpleSlides extends HTMLElement {
   // Use this function to advance to the next image or loop backl to the first image. 
   _nextImg() {
     // Add 1 to index if index is equal to the length of _imgs set it to 0. 
+    this._index += 1
+    if (this._index >= this._imgs.length) {
+      this._index = 0
+    }
+
     // Set translate the inner div to width * index * -1
     // You need to translate the slides one slide width to the left 
     // each time this method is called. 
+    this._inner.style.transform = `translate(${(this._width * this._index) * -1}px)`
+    this._inner.style.transition = `all ${this._transition}ms`
 
   }
 }
